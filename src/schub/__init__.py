0d0dbb53ed931a26104c55b808df86a0091e0e86
HaidYi <haidyi@cs.unc.edu>
2024-04-02 16:18:51 -0400

    change to dynamic version

--- src/schub/__init__.py
+++ src/schub/__init__.py
@@ -1,13 +1,19 @@
 import sys
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("schub")
+
+except PackageNotFoundError:
+    try:
+        from ._version import version as __version__
+    except ModuleNotFoundError:
+        raise RuntimeError("schub is not installed. Please install it with `pip install schub`. ")
+
 
 from ._constants import REGISTRY_KEYS
 from . import data, utils, tl, model
 from . import preprocessing as pp
-
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "unknown"
 
 sys.modules.update({f"{__name__}.{m}": globals()[m] for m in ["pp"]})
 
